5b9bed3fa46a0eaff7b5d51e90eb42e5b8561a9a
Edison Kashari <edisonkashari@hotmail.com>
2022-03-04 15:01:13 -0500

    day8_properties_config_reader

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -31,7 +31,7 @@ public class T4_Config_Practice {
         //3- Write “apple” in search box
         WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
 
-        googleSearchBox.sendKeys("apple" + Keys.ENTER);
+        googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
 
         String expectedTitle = "apple - Google Search";
         String actualTitle = driver.getTitle();
