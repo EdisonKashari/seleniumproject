a38aad9675bc7acd5b760afad74bf84229bf9b8d
Edison Kashari <edisonkashari@hotmail.com>
2022-03-05 21:32:13 -0500

    day9_driver

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -1,19 +1,15 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
 public class T4_Config_Practice {
-    public WebDriver driver;
+  /*  public WebDriver driver;
 
     @BeforeMethod
     public void setupMethod() {
@@ -23,18 +19,23 @@ public class T4_Config_Practice {
         driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get("https://www.google.com");
+
     }
+
+
+   */
+
 
     @Test
     public void google_search_test() {
+        Driver.getDriver().get("https://www.google.com");
         //3- Write “apple” in search box
-        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
 
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
 
         String expectedTitle = "apple - Google Search";
-        String actualTitle = driver.getTitle();
+        String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertEquals(expectedTitle, actualTitle);
 
---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driver/Singleton.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.day9_javafaker_testbase_driver;
+
+public class Singleton {
+    private Singleton(){
+
+
+
+    }
+    private static String word;
+
+}
